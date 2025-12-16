87fa0ab5c7d980b94245cb5acd3bd86fd87b657a
miggy88 <cunananluismiguel11@gmail.com>
2025-12-16 09:14:26 +0800

    1232

---
+++ ThreadPriority/MyThreadClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPriority
+{
+    class MyThreadClass
+    {
+        static void Thread1()
+        {
+
+            for (int loopcount = 1; loopcount < 2; loopcount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread " + thread.Name + " = " + loopcount );
+                Thread.Sleep(500);
+
+
+            }
+
+      
+
+
+
+        }
+
+        static void Thread2()
+        {
+            for (int loopcount = 1; loopcount < 6; loopcount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread " + thread.Name + " = " + loopcount);
+                Thread.Sleep(1500);
+
+            }
+
+
+        }
+    }
+}
--- ThreadPriority/frmTrackThread.cs
+++ ThreadPriority/frmTrackThread.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,13 +13,62 @@ namespace ThreadPriority
 {
     public partial class frmTrackThread: Form
     {
+        Thread threadA;
+        Thread threadB;
+        Thread threadC;
+        Thread threadD;
+
+
         public frmTrackThread()
         {
             InitializeComponent();
         }
 
+        static void ThreadA()
+        {
+            Console.WriteLine("Thread A - Highest Priority");
+        }
+
+        static void ThreadB()
+        {
+            Console.WriteLine("Thread B - Normal Priority");
+        }
+
+        static void ThreadC()
+        {
+            Console.WriteLine("Thread C - Above Normal Priority");
+        }
+
+        static void ThreadD()
+        {
+            Console.WriteLine("Thread D - Below Normal Priority");
+        }
+
+
+
+
         private void frmTrackThread_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            threadA = new Thread(ThreadA);
+            threadB = new Thread(ThreadB);
+            threadC = new Thread(ThreadC);
+            threadD = new Thread(ThreadD);
+
+            threadA.Priority = System.Threading.ThreadPriority.Highest;
+            threadB.Priority = System.Threading.ThreadPriority.Normal;
+            threadC.Priority = System.Threading.ThreadPriority.AboveNormal;
+            threadD.Priority = System.Threading.ThreadPriority.BelowNormal;
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
 
         }
     }
